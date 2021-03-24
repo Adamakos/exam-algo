33a0b89133df354bc61a53d90b26d61107cc1920
Adamakos <adamhohmann@gmail.com>
2021-03-24 15:15:24 +0100

    Add sum

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -5,6 +5,13 @@ import java.util.List;
 public class OddNumbers {
 
     public static Integer sum(List<Integer> numbers) {
-        return null;
+
+        Integer numSum = 0;
+
+        for (Integer number : numbers) {
+            if (number % 2 != 0) { numSum += number; }
+        }
+
+        return numSum;
     }
 }
